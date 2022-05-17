32a71781721190f664f1f9954c6efcd31b7bd14d
alexdimitrov2000 <alex_dimitrov2000@abv.bg>
2022-05-17 15:25:29 +0300

    Implemented routing.

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import setupDatabase from "./src/data/database";
+import setupRouter from "./src/routes";
 
 startServer();
 
@@ -10,20 +11,15 @@ async function startServer() {
     const app = express();
 
     app.use(express.json());
-
-    app.get("/", (req, res) => {
-        console.log("Working!");
-
-        res.status(200).send("server is ok");
-    })
-
-    app.get("/forbidden", (req, res) => {
-        res.status(403).send("FORBIDDEN!")
-    });
-
+    
+    // Establishing database connection
     const dbMessage = await setupDatabase();
     console.log(dbMessage);
 
+    // Route configuration
+    setupRouter(app);
+    
+    // Starting the server
     app.listen(process.env.PORT, () => {
         console.log(`Server listening on port ${process.env.PORT}...`)
     });
---
+++ server/src/routes/index.ts
@@ -0,0 +1,6 @@
+import { Application } from "express";
+import authController from "../controllers/auth.controller";
+
+export default function setup(app: Application) : void {
+    app.use("/auth", authController);
+}
