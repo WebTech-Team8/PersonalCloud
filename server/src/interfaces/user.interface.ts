60dfaf08fee7e563b9b2c36acccb6967bd71f6cd
alexdimitrov2000 <alex_dimitrov2000@abv.bg>
2022-05-17 14:12:13 +0300

    Implemented User and Token interfaces.

---
+++ server/src/interfaces/user.interface.ts
@@ -0,0 +1,14 @@
+import { Request } from "express";
+
+export interface IUser {
+    id: string;
+    username: string;
+    password: string;
+    email: string;
+    firstName: string;
+    lastName: string;
+}
+
+export interface IAuthenticatedUserRequest extends Request {
+    user: IUser;
+}
