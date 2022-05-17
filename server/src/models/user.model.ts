50c90570d1b509f97019d97c3d1f5b87a887ba9b
alexdimitrov2000 <alex_dimitrov2000@abv.bg>
2022-05-17 14:12:21 +0300

    Implemented User model.

---
+++ server/src/models/user.model.ts
@@ -0,0 +1,28 @@
+import { model, Schema } from "mongoose";
+import { IUser } from "../interfaces/user.interface";
+
+const userSchema = new Schema<IUser>({
+    id: Schema.Types.ObjectId,
+    username: {
+        type: Schema.Types.String,
+        required: true
+    },
+    password: {
+        type: Schema.Types.String,
+        required: true
+    },
+    email: {
+        type: Schema.Types.String,
+        required: true
+    },
+    firstName: {
+        type: Schema.Types.String,
+        required: true
+    },
+    lastName: {
+        type: Schema.Types.String,
+        required: true
+    }
+});
+
+export const UserModel = model<IUser>("User", userSchema);
