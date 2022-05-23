95d93c5d01311c85894325654f3654464921275c
alexdimitrov2000 <alex_dimitrov2000@abv.bg>
2022-05-23 15:11:15 +0300

    Implemented tokens service.

---
+++ server/src/services/tokens.service.ts
@@ -0,0 +1,9 @@
+import { RefreshTokenModel } from "../models/token.model";
+
+async function removeRefreshToken(refreshToken: string) {
+    return await RefreshTokenModel.findOneAndRemove({ refreshToken });
+}
+
+export {
+    removeRefreshToken
+}
