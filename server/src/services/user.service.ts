81423da8a2df8a81fdd5c7f592a43df7d5ccb8ce
alexdimitrov2000 <alex_dimitrov2000@abv.bg>
2022-05-23 15:11:07 +0300

    Implemented user service.

---
+++ server/src/services/user.service.ts
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+import { IUser } from "../interfaces/user.interface";
+import { UserModel } from "../models/user.model";
+
+async function createUser(userData: IUser) {
+    const user = new UserModel({
+        id: new mongoose.Types.ObjectId(),
+        username: userData.username,
+        password: userData.password,
+        email: userData.email,
+        firstName: userData.firstName,
+        lastName: userData.lastName
+    });
+
+    const validation = user.validateSync();
+    if (validation) {
+        throw validation;
+    }
+
+    await user.save();
+
+    return user;
+}
+
+async function getByEmail(email: string) {
+    return await UserModel.findOne({ email });
+}
+
+export {
+    createUser,
+    getByEmail
+}
