d6b36ca566ef1ea1e45a5a59d46461e0c334abe2
alexdimitrov2000 <alex_dimitrov2000@abv.bg>
2022-06-07 21:21:11 +0300

    Added routes for directories and files.

---
+++ server/src/controllers/file.controller.ts
@@ -0,0 +1,5 @@
+import { Router } from "express";
+
+const fileController = Router();
+
+export default fileController;
--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,10 +1,14 @@
 import { Router, Application } from 'express';
 import authController from '../controllers/auth.controller';
+import directoryController from '../controllers/directory.controller';
+import fileController from '../controllers/file.controller';
 
 const router = Router();
 
 export default function setup(app: Application, path: string) : void {
     router.use('/auth', authController);
+    router.use('/directories', directoryController);
+    router.use('/files', fileController);
 
     app.use(path, router);
 }
