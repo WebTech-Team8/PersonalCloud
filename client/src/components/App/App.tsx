f30a659bc05a1cd3b7646ec3cc525163e7bbae26
alexdimitrov2000 <alex_dimitrov2000@abv.bg>
2022-06-08 19:30:35 +0300

    Added styles to the App component and reconfigured some of the default styles.

--- client/src/components/App/App.tsx
+++ client/src/components/App/App.tsx
@@ -1,33 +1,29 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import logo from '../../logo.svg';
+import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
 import './App.css';
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="App">
+      <div className="site">
         <Header isLogged={false} />
         <Switch>
           <Route path="/" exact>
-            <main className="App-header">
+            <main className="site-main">
               <img src={logo} className="App-logo" alt="logo" />
               <p>
                 Edit <code>src/App.tsx</code> and save to reload.
               </p>
-              <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
+              <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
                 Learn React
               </a>
             </main>
           </Route>
           <Route path="/login" exact>
-            <main className='App-header'>
+            <main className='site-main'>
               <img src={logo} className="App-logo" alt="logo" />
               <p>
                 Login Page
@@ -35,7 +31,7 @@ function App() {
             </main>
           </Route>
           <Route path="/register" exact>
-            <main className='App-header'>
+            <main className='site-main'>
               <img src={logo} className="App-logo" alt="logo" />
               <p>
                 Register Page
@@ -43,6 +39,7 @@ function App() {
             </main>
           </Route>
         </Switch>
+        <Footer />
       </div>
     </BrowserRouter>
   );
