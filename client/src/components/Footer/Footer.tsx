51e34fa85078777c355c2384edfe344641e60145
alexdimitrov2000 <alex_dimitrov2000@abv.bg>
2022-06-08 19:30:07 +0300

    Implemented Footer component with styles.

---
+++ client/src/components/Footer/Footer.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Footer.css'
+
+const Footer: React.FC = () => {
+    return (
+        <footer className="site-footer">
+            <div className="wrapper">
+                <p>Personal Cloud</p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer; 
