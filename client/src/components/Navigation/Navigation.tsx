7f95ac454cb448c7799ac1beb6cf6a36eb15f6c5
alexdimitrov2000 <alex_dimitrov2000@abv.bg>
2022-06-08 19:26:20 +0300

    Added styles to the Navigation component.

--- client/src/components/Navigation/Navigation.tsx
+++ client/src/components/Navigation/Navigation.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import { NavigationComponentProps } from './Navigation.props';
+import './Navigation.css'
 
 const Navigation: React.FC<NavigationComponentProps> = ({ isLogged }) => {
     return (
