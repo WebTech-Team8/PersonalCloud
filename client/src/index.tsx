64f062ccb06e4eff9080506ac9fe4d1ad76c8b9e
alexdimitrov2000 <alex_dimitrov2000@abv.bg>
2022-06-08 19:25:59 +0300

    Added resetting styles and some variables.

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './styles.reset.css';
+import './styles.variables.css';
 import App from './components/App/App';
 
 const root = ReactDOM.createRoot(
