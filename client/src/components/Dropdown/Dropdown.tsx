1aa13b604a48a0b3cce10e6ca8e5925bc3f99fbb
alexdimitrov2000 <alex_dimitrov2000@abv.bg>
2022-06-17 11:45:35 +0300

    Made the navbar dropdown hoverable.

--- client/src/components/Dropdown/Dropdown.tsx
+++ client/src/components/Dropdown/Dropdown.tsx
@@ -6,13 +6,9 @@ import { faCaretDown } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom';
 
 const Dropdown: React.FC<DropdownProps> = ({ btnName, options }) => {
-    const showContent = () => {
-        document.getElementsByClassName('dropdown-content')[0].classList.toggle('show');
-    }
-
     return (
         <div className="dropdown">
-            <button onClick={showContent} className="dropdown-btn">{btnName} <FontAwesomeIcon icon={faCaretDown} /></button>
+            <button className="dropdown-btn">{btnName} <FontAwesomeIcon icon={faCaretDown} /></button>
             <div className="dropdown-content">
                 { options.map(o => <Link to='#' onClick={o.onclick} key={o.name}>{o.name}</Link>) }
             </div>
